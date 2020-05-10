796cd1fd3da81f40b5800e55847544970deeb918
IntlUser <dyuzhev@gmail.com>
2020-05-10 11:39:16 +0300

    Exception fix

--- src/main/java/com/metanorma/fop/mn2pdf.java
+++ src/main/java/com/metanorma/fop/mn2pdf.java
@@ -204,7 +204,7 @@ public class mn2pdf {
             System.exit(ERROR_EXIT_CODE);
         }
         // flush temporary folder
-        if (!DEBUG) {
+        if (!DEBUG && Files.exists(tmpfilepath)) {
             //Files.deleteIfExists(tmpfilepath);
             try {
                 Files.walk(tmpfilepath)
