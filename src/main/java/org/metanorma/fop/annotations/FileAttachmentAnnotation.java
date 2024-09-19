423f6aab75b32100306ecce37c99e1d37b9ce3b8
Alexander Dyuzhev <dyuzhev@gmail.com>
2024-09-19 23:13:38 +0300

    removing embedded files added, #267

---
+++ src/main/java/org/metanorma/fop/annotations/FileAttachmentAnnotation.java
@@ -0,0 +1,94 @@
+package org.metanorma.fop.annotations;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentNameDictionary;
+import org.apache.pdfbox.pdmodel.PDEmbeddedFilesNameTreeNode;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.common.filespecification.PDComplexFileSpecification;
+import org.apache.pdfbox.pdmodel.common.filespecification.PDFileSpecification;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
+import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationFileAttachment;
+import org.metanorma.utils.LoggerHelper;
+import java.io.*;
+import java.util.*;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Alexander Dyuzhev
+ */
+public class FileAttachmentAnnotation {
+    
+    protected static final Logger logger = Logger.getLogger(LoggerHelper.LOGGER_NAME);
+    
+    private boolean DEBUG = false;
+    
+    public void process(File pdf) throws IOException {
+        PDDocument document = null;
+        
+        try {
+            document = PDDocument.load(pdf);
+
+            ArrayList<String> embeddedFileAnnotations = new ArrayList<>();
+
+            int numberOfPages = document.getNumberOfPages();
+            for (int pageIndex = 0; pageIndex < numberOfPages; pageIndex++) {
+                PDPage page = document.getPage(pageIndex);
+                List<PDAnnotation> annotations = page.getAnnotations();
+                for(int annotataionIndex = 0; annotataionIndex < annotations.size(); annotataionIndex++) {
+                    PDAnnotation annotation = annotations.get(annotataionIndex);
+                    if (annotation instanceof PDAnnotationFileAttachment) {
+                        annotation.constructAppearances();
+                        PDFileSpecification f = ((PDAnnotationFileAttachment) annotation).getFile();
+                        embeddedFileAnnotations.add(f.getFile());
+                        annotations.set(annotataionIndex, annotation);
+                    }
+
+                }
+                /*for (PDAnnotation annotation: annotations) {
+                    if (annotation instanceof PDAnnotationFileAttachment) {
+                        annotation.constructAppearances();
+                    }
+                }*/
+                document.getPage(pageIndex).setAnnotations(annotations);
+            }
+
+            // remove attachments which have FileAttachment annotation equivalent
+            PDDocumentNameDictionary namesDictionary = new PDDocumentNameDictionary(document.getDocumentCatalog());
+            PDEmbeddedFilesNameTreeNode efTree = namesDictionary.getEmbeddedFiles();
+            if (efTree != null)
+            {
+                Map<String, PDComplexFileSpecification> names = efTree.getNames();
+                Map<String, PDComplexFileSpecification> newnames = new HashMap<>();
+
+                Iterator<Map.Entry<String,PDComplexFileSpecification>> iter = names.entrySet().iterator();
+                while (iter.hasNext()) {
+                    Map.Entry<String,PDComplexFileSpecification> entry = iter.next();
+                    PDComplexFileSpecification fileSpec = entry.getValue();
+                    String embeddedFileName = fileSpec.getFile();
+                    if(!embeddedFileAnnotations.contains(embeddedFileName)){
+                        newnames.put(entry.getKey(), entry.getValue());
+                    }
+                }
+
+                efTree.setNames(newnames);
+                namesDictionary.setEmbeddedFiles(efTree);
+            }
+
+            document.save(pdf);
+                
+        } catch (IOException ex) {
+            logger.severe("Can't read annotation data from PDF.");
+            ex.printStackTrace();
+        }
+
+
+        finally {
+            if( document != null ) {
+                document.close();
+            }
+        }
+        
+    }
+    
+}
